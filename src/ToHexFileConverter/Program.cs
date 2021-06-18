106c58a890a8c6a76d6e7f9bad118c88abb29077
OtmarP (opMedion14b) <Otmar.Pilgerstorfer@chello.at>
2021-06-18 10:58:53 +0200

    Code Format #1

--- src/ToHexFileConverter/Program.cs
+++ src/ToHexFileConverter/Program.cs
@@ -8,44 +8,44 @@ namespace ToHexFileConverter
     class Program
     {
         [STAThread]
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
             OpenFileDialog dialog = new OpenFileDialog();
 
-            if( dialog.ShowDialog() == DialogResult.OK )
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
-                    DumpHex( dialog.FileName, dialog.FileName + ".txt" );
+                    DumpHex(dialog.FileName, dialog.FileName + ".txt");
                 }
-                catch( Exception ex )
+                catch (Exception ex)
                 {
-                    Console.WriteLine( "Exception: " + ex );
+                    Console.WriteLine("Exception: " + ex);
                 }
             }
         }
 
-        private static void DumpHex( string binaryFile, string hexDumpFile )
+        private static void DumpHex(string binaryFile, string hexDumpFile)
         {
             bool finished = false;
             int readByte = 0;
 
-            using( StreamWriter writer = new StreamWriter( hexDumpFile ) )
-            using( FileStream reader = File.OpenRead( binaryFile ) )
+            using (StreamWriter writer = new StreamWriter(hexDumpFile))
+            using (FileStream reader = File.OpenRead(binaryFile))
             {
-                while( !finished )
+                while (!finished)
                 {
-                    if( (readByte = reader.ReadByte()) < 0 )
+                    if ((readByte = reader.ReadByte()) < 0)
                     {
                         finished = true;
                     }
 
-                    if( !finished )
+                    if (!finished)
                     {
                         // write out byte value
-                        string b = string.Format( "{0:x2}", readByte );
-                        writer.Write( b );
-                        Console.Write( b );
+                        string b = string.Format("{0:x2}", readByte);
+                        writer.Write(b);
+                        Console.Write(b);
                     }
                 }
             }
